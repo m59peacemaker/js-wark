44b9a24063904fabb0fca31ff1be23568cfb8976
Johnny Hauser <johnnyhauser@gmail.com>
2020-03-08 00:44:27 -0600

    test behaviors, fix some small bugs

--- src/Behavior/index.spec.js
+++ src/Behavior/index.spec.js
@@ -1,91 +1,70 @@
 import { test } from 'zora'
 import * as Behavior from './'
 import * as Event from '../Event'
-import { add, identity, collectValues } from '../utils'
-import { Time } from '../Time'
+import * as Dynamic from '../Dynamic'
+import { add, identity, collectValues } from '../util'
 
 test('Behavior', async t => {
-	await t.test('behavior.sample()', t => {
+	t.test('behavior.sample() returns the same value for the same t and next value for the next t', t => {
 		const values = [ 'a', 'b', 'c' ]
-		const time = Time()
-		const b = Behavior.create(time, () => values.shift())
-		t.equal(time.current(), 0)
-		t.equal(b.sample(), 'a')
-		t.equal(b.sample(), 'a')
-		time.forward()
-		t.equal(time.current(), 1)
-		t.equal(b.sample(), 'b')
-		t.equal(b.sample(), 'b')
-		t.equal(b.sample(), 'b')
-		time.forward()
-		t.equal(b.sample(), 'c')
+		const b = Behavior.create(() => values.shift())
+		const t0 = Symbol()
+		t.equal(b.sample(t0), 'a')
+		t.equal(b.sample(t0), 'a')
+		const t1 = Symbol()
+		t.equal(b.sample(t1), 'b')
+		t.equal(b.sample(t1), 'b')
+		t.equal(b.sample(t1), 'b')
+		const t2 = Symbol()
+		t.equal(b.sample(t2), 'c')
 	})
 
-	await t.test('Behavior.proxy', t => {
-		const time = Time()
-		const event = Event.create(time)
-		const proxy = Behavior.proxy(time)
-		const snapshot = Event.snapshot (b => a => [ ...b, a ]) (proxy) (event)
-		const hold = proxy.mirror (Behavior.hold ([]) (snapshot))
-		time.start()
-		t.deepEqual(hold.sample(), [])
-		event.emit(1)
-		t.deepEqual(hold.sample(), [ 1 ])
-		event.emit(2)
-		t.deepEqual(hold.sample(), [ 1, 2 ])
+	t.test('Behavior.map', t => {
+		const values = [ 0, 1 ]
+		const b = Behavior.map (add(1)) (Behavior.create(() => values.shift()))
+		const t0 = Symbol()
+		t.equal(b.sample(t0), 1)
+		t.equal(b.sample(t0), 1)
+		const t1 = Symbol()
+		t.equal(b.sample(t1), 2)
+		t.equal(b.sample(t1), 2)
 	})
-	await t.test('Behavior.of creates a constant', t => {
-		const b = Behavior.of(0)
+
+	t.test('Behavior.constant', t => {
+		const b = Behavior.constant(0)
 		t.deepEqual(
-			[ b.sample(), b.sample() ],
+			[ b.sample(Symbol()), b.sample(Symbol()) ],
 			[ 0, 0 ]
 		)
 	})
-	await t.test('Behavior.map', t => {
-		const time = Time()
-		const values = [ 0, 1 ]
-		const b = Behavior.map (add(1)) (Behavior.create(time, () => values.shift()))
-		t.equal(b.sample(), 1)
-		t.equal(b.sample(), 1)
-		time.forward()
-		t.equal(b.sample(), 2)
-		t.equal(b.sample(), 2)
-	})
 
-	await t.test('Behavior.lift', t => {
-		const time = Time()
-		const b = Behavior.lift ((a, b) => a + b) ([ Behavior.of(3), Behavior.of(6) ])
+	t.test('Behavior.lift', t => {
+		const b = Behavior.lift ((a, b) => a + b) ([ Behavior.constant(3), Behavior.constant(6) ])
 		t.equal(b.sample(), 9)
 	})
 
-	await t.test('Behavior.fold', t => {
-		const time = Time()
-		const e = Event.create(time)
-		const b = Behavior.fold (a => b => [ ...a, b ]) ([]) (e)
-		time.start()
-		t.equal(b.sample(), [])
-		e.emit(1)
-		t.equal(b.sample(), [ 1 ])
-		e.emit(4)
-		t.equal(b.sample(), [ 1, 4 ])
-		e.emit(10)
-		t.equal(b.sample(), [ 1, 4, 10 ])
-	})
-
-	await t.test('two events occurring at the same time and tagged with the same behavior have the same value', t => {
-		const time = Time()
-		const eventA = Event.create(time)
-		const randomBehavior = Behavior.create(time, () => Math.random())
+	t.test('two events occurring at the same time and tagged with the same behavior have the same value', t => {
+		const eventA = Event.create()
+		const randomBehavior = Behavior.create(Math.random)
 		const randomEventA1 = Event.tag (randomBehavior) (eventA)
 		const randomEventA2 = Event.tag (randomBehavior) (eventA)
 
 		const actualA1 = collectValues(randomEventA1)
 		const actualA2 = collectValues(randomEventA2)
 
-		eventA.emit(1)
-		eventA.emit(2)
-		eventA.emit(3)
+		eventA.occur(1)
+		eventA.occur(2)
+		eventA.occur(3)
 
 		t.deepEqual(actualA1(), actualA2(), actualA1())
+	})
+
+	t.test('Behavior.proxy', t => {
+		const proxy = Behavior.proxy()
+		const r = proxy.mirror(Behavior.create(Math.random))
+		const t0 = Symbol()
+		const t1 = Symbol()
+		t.equal(r.sample(t0), proxy.sample(t0))
+		t.equal(r.sample(t1), proxy.sample(t1))
 	})
 })
