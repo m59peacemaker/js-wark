9ce7df3a936675c01e85beecf265921abbeb8cec
Johnny Hauser <johnnyhauser@gmail.com>
2020-03-07 23:02:19 -0600

    tidy up util, fix pipe fn, test pipe fn

---
+++ src/util.spec.js
@@ -0,0 +1,13 @@
+import { test } from 'zora'
+import { pipe } from './util'
+
+test('pipe', t => {
+	t.equal(
+		pipe ([ v => v + 1, v => v * 2 ]) (1),
+		4
+	)
+	t.equal(
+		pipe ([ v => v ]) (0),
+		0
+	)
+})
