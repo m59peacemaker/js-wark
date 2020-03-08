8a3ef6c54ff58daa4742cd900e29e372faae83af
Johnny Hauser <johnnyhauser@gmail.com>
2020-03-08 00:44:27 -0600

    test behaviors, fix some small bugs

--- src/Behavior/index.js
+++ src/Behavior/index.js
@@ -1,10 +1,10 @@
 import { call } from '../util'
 
 const Behavior = f => {
-	const cache = { time: Symbol(), value: null }
+	const cache = { t: Symbol(), value: null }
 	return {
-		sample: time => {
-			cache.time === time || Object.assign(cache, { time, value: f() })
+		sample: t => {
+			cache.t === t || Object.assign(cache, { t, value: f(t) })
 			return cache.value
 		}
 	}
@@ -18,9 +18,9 @@ export const chain = f => b => create(t => f(b.sample(t)).sample(t))
 
 export const lift = f => behaviors => create(t => f(...behaviors.map(b => b.sample(t))))
 
-export const lift2 = f => a => b => lift (f) ([ a, b ])
+export const lift2 = f => a => b => lift (a => b => f (a) (b)) ([ a, b ])
 
-export const apply = bf => bv => lift (call) ([ bf, bv ])
+export const apply = bf => bv => lift2 (call) ([ bf, bv ])
 
 export const map = f => b => create(t => f(b.sample(t)))
 
@@ -30,7 +30,7 @@ export const createProxy = ({ pre_mirror_sample_error_message }) => {
 		sample = behavior.sample
 		return behavior
 	}
-	return { sample: () => sample(), mirror }
+	return { sample: t => sample(t), mirror }
 }
 
-export const proxy = () => create_proxy({ pre_mirror_sample_error_message: 'Behavior proxy should not be sampled before being mirrored!' })
+export const proxy = () => createProxy({ pre_mirror_sample_error_message: 'Behavior proxy should not be sampled before being mirrored!' })
