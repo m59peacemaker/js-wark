45894d4d190a1b1952ca448deb96b006d98b7ea5
Johnny Hauser <johnnyhauser@gmail.com>
2020-03-13 17:00:12 -0500

    rename combineByLeftmost to combineAllByLeftmost and make combineByLeftmost (a) (b)

--- src/Event/index.spec.js
+++ src/Event/index.spec.js
@@ -223,11 +223,11 @@ test('Event', t => {
 		})
 	})
 
-	t.test('Event.combineByLeftmost', t => {
+	t.test('Event.combineAllByLeftmost', t => {
 		t.test('concatting events that never occur together occurs with the value of each event occurrence', t => {
 			const a = Event.create()
 			const b = Event.create()
-			const c = Event.combineByLeftmost([ a, b ])
+			const c = Event.combineAllByLeftmost([ a, b ])
 			const actual = collectValues(c)
 			b.occur(10)
 			a.occur(20)
@@ -239,7 +239,7 @@ test('Event', t => {
 			const a = Event.create()
 			const b = Event.filter (v => v < 10 || (v !== 10 && v % 2 === 0)) (a)
 			const c = Event.filter (v => v > 10 || (v !== 10 && v % 2 === 0)) (a)
-			const d = Event.combineByLeftmost([ b, c ])
+			const d = Event.combineAllByLeftmost([ b, c ])
 			const actual = collectValues(d)
 			a.occur(9)  // b, < 10
 			a.occur(10)
@@ -251,7 +251,7 @@ test('Event', t => {
 		t.test('concatting events that always occur together always occurs the value of the first given event', t => {
 			const a = Event.create()
 			const b = Event.map (add(1)) (a)
-			const c = Event.combineByLeftmost([ a, b ])
+			const c = Event.combineAllByLeftmost([ a, b ])
 			const actual = collectValues(c)
 			a.occur(20)
 			a.occur(30)
