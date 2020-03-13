296340f1f3c9be5d83e174d73dbcba362f3aeb70
Johnny Hauser <johnnyhauser@gmail.com>
2020-03-13 17:33:06 -0500

    typo

--- src/Event/index.js
+++ src/Event/index.js
@@ -3,7 +3,7 @@ import { add, identity, noop } from '../util'
 
 /*
 	This is shared, but always unique due to Symbol. It would not cause any difference in behavior if two instances of the library came into contact.
-	The number inside the symbol arbitrary and just for debugging and testing. The library never examines it and no application could should ever examine it.
+	The number inside the symbol arbitrary and just for debugging and testing. The library never examines it and no application should ever examine it.
 	The symbol acts as a unique identifier to the propagation from a source event and is used to interact with the cache in behaviors so that the behavior can be sampled multiple times within a propagation and return the same value each time.
 */
 const nextTime = (n => () => Symbol(n++))(0)
