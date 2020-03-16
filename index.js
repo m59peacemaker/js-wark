fc89395abaafd33e5181448e42d15915deaf6f73
Johnny Hauser <johnnyhauser@gmail.com>
2020-03-16 15:47:21 -0500

    src/index.js should be main

--- index.js
+++
@@ -1,3 +0,0 @@
-export * as Event from './src/Event'
-export * as Behavior from './src/Behavior'
-export * as Dynamic from './src/Dynamic'
---
+++ src/index.js
@@ -0,0 +1,3 @@
+export * as Event from './Event'
+export * as Behavior from './Behavior'
+export * as Dynamic from './Dynamic'
