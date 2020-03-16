d609cfd82d33f9318166f4d33654d8bb0a3643dc
Johnny Hauser <johnnyhauser@gmail.com>
2020-03-16 15:50:56 -0500

    remove unused dist/

--- dist/utils.js
+++
@@ -1,6 +0,0 @@
-var noop = function () { return; };
-var identity = function (v) { return v; };
-var pipe = function (fns) { return fns.reduce(function (acc, fn) { return function (v) { return acc(fn(v)); }; }); };
-var add = function (a) { return function (b) { return a + b; }; };
-var isPromise = function (v) { return typeof v.then === 'function'; };
-export { noop, identity, pipe, add, isPromise };
