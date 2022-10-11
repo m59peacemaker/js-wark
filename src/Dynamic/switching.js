4a2b9173f455b2c43417f3cfbb0e7629ec9e444e
Johnny Hauser <johnnyhauser@gmail.com>
2022-10-11 11:38:29 -0500

    Dynamic.switching, NOTE: probably a wrong implementation due getting the input dynamic's value without providing an instant

---
+++ src/Dynamic/switching.js
@@ -0,0 +1,7 @@
+import { immediately } from '../immediately.js'
+import { switch_updating } from '../Event/switch_updating.js'
+import { updates } from './updates.js'
+import { get } from './get.js'
+
+// TODO: THIS IS WRONG! Using `get` here will needlessly cause the dynamic and its upstream dynamics to recompute (or worse, upstream plain samples, if that's a thing somehow)
+export const switching = dynamic => switch_updating (immediately) (get (dynamic)) (updates (dynamic))
